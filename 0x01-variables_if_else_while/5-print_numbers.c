e8846ebe780cd3ee64c89112fe8f9338c89cf122
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-03-17 16:43:24 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this is a fct that prints alphabets exept e and q
+ * Return: 0 (success)
+ */
+int main(void)
+{	int i;
+	for (i = 0; i < 10 ; i++)
+		printf("%d", i);
+	putchar('\n');
+	return (0);
+}
