91ecb0f363abff47bfbb54684dbe8e452c0dbfe6
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-03-22 09:43:34 -0700

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,10 +6,11 @@
 void times_table(void)
 {
 	int i, j, k;
+
 	for (i = 0 ; i <= 9 ; i++)
 	{
 		for (j = 0 ; j <= 9 ; j++)
-		{	
+		{
 			k = i * j;
 
 			if (j == 0)
@@ -22,7 +23,8 @@ void times_table(void)
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
-		}else if (k >= 10)
+
+		} else if (k >= 10)
 		{
 			_putchar(',');
 			_putchar(' ');
