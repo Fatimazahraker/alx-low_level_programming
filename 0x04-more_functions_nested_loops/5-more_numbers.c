e9d6b408adf95478af14512117906240f8a67faa
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-03-23 14:37:10 -0700

    more

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,15 +15,9 @@ void more_numbers(void)
 
 	for (i = 0 ; i <= 14 ; i++)
 	{
-		if (i <= 9)
-		{
-			_putchar('0' + i);
-		}
 		if (i > 9)
-		{
-			_putchar('0' + i / 10);
-			_putchar('0' + i % 10);
-		}
+		_putchar('1');
+		_putchar('0' + i % 10);
 	}
 	_putchar('\n');
 	}
