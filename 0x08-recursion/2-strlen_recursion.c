1c7d1e32438393e1e06d982f64a367bd1af73e3c
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-04-05 13:35:31 -0700

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -14,9 +14,7 @@ int _strlen_recursion(char *s)
 
 	if (*s != '\0')
 	{
-	
 		l = 1 + _strlen_recursion(s + 1);
 	}
-	
 	return (l);
 }
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _sqrt_recursion: returns the natural square root of a number
+ * @n:integer
+ * Return: sgrt root of n or -1
+ */
+
+int _sqrt_recursion(int n)
+{
+	if ( n % n 
+
