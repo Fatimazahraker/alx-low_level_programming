d2264b424a3b90b9b5905177e471daf01f69f048
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-04-06 08:56:34 -0700

    task 100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <stdio.h>
+int identity(char *s1, int k, int size);
+int length(char *str);
 
 /**
  * is_palindrome - ckeck if a string is a palindrome
@@ -9,12 +11,36 @@
 
 int is_palindrome(char *s)
 {
-	while (s[i] != '\0')
+	if (*s == 0)
+		return (1);
+	return (identity(s, 0, length(s)));
+}
+
+/**
+ * length - return the length of string
+ * @str: string
+ * Return: the lengthof string
+ */
+int length(char *str)
+{
+	if (*str != '\0')
 	{
-		i++;
+		return (1 + length(str + 1));
 	}
-
-	if (l != i / 2)
-	{
-		if 
-
+	return (0);
+}
+/**
+ * identity - check the identity of palindrome
+ * @s1: string to check
+ * @k: iterator
+ * @size: the lenght of s1
+ * Return: return1 if palindrome, 0 if not
+ */
+int identity(char *s1, int k, int size)
+{
+	if (*(s1 + k) != *(s1 + size - 1))
+		return (0);
+	if (k >= size)
+		return (1);
+	return (identity(s1, k + 1, size - 1));
+}
