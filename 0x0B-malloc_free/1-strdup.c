916ab4d6ab6c91500688073870e8aebf65f1a04a
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-04-11 14:14:12 -0700

    task1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,4 +3,35 @@
 #include <stdlib.h>
 
 /**
- * _strdup - 
+ * _strdup - allocated space in memory, contains a copy of the string
+ * @str: string
+ * return: poiter to string or NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	int size, i;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (size = 0; str[size] != '\0'; size++)
+	{
+	
+	ptr = malloc(sizeof(*str) * size + 1);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+	for (i = 0; str[i] == '\0'; i++)
+	{
+		ptr[i] = str[i];
+	}
+	}
+	}
+	return(ptr);
+}
+		
