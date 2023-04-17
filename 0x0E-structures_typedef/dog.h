46daa866711ab5fa0650aaced894f55450ceadd1
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-04-17 14:24:47 -0700

    task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ *
+ * @name: name of  dog
+ * @age: age of the dog
+ * @owner: dog owner
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif /* struct dog */
