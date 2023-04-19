f89565c70fa89370307f43c2f9a2f65584d0a709
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-04-19 13:54:18 -0700

    task

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,16 +16,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array != NULL && size > 0 && cmp != NULL)
-	{
-		if (size <= 0)
+	if (array == NULL || cmp == NULL || size <= 0)
 			return (-1);
 
 		for (i = 0; i < 0; i++)
 		{
-			if (cmp(array[i]) == 1)
+			if (cmp(array[i]))
 				return (i);
 		}
-	}
 	return (-1);
 }
