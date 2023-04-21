a345928f6b249060727a4c705f164994ec64c1cc
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-04-21 15:10:34 -0700

    task

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
+	num2 = atoi(argv[3]);
 	op = (*get_op_func(argv[2]))(num1, num2);
 	printf("%d\n", op);
 	return (0);
