171b2e8f078479b89b7c4026f16b3e083aef5dda
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-05-04 15:15:12 -0700

    task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,11 +8,17 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n = 0, i = 0;
+	unsigned int n = 0, i;
 
 	if (b == NULL)
 		return (0);
 
+	for (i = 0 ; b[i] != '\0' ; i++)
+	{
+		if (b[i] != '1' && b[i] != '0')
+			return (0);
+	}
+	i = 0;
 	while (b[i] == '0' || b[i] == '1')
 	{
 		n <<= 1;
