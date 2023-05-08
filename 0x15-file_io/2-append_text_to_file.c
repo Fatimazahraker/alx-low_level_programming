612c00b2ee0b9ea812def7d7fced7aa161f296e3
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-05-08 15:09:38 -0700

    task

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,4 +9,21 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int w, o, i = 0;
 
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[i] != '\0')
+		{
+			i++;
+		}
+	}
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, i);
+	if (o == -1 || w == -1)
+		return (-1);
+	close(o);
+	return (1);
+}
