33e762d158dc18e1c095a2a231176b0da8d53bdd
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-10-10 00:34:27 -0700

    task

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * linear_search - search for value in an array
+ * array: pointer to array
+ * size: size of array
+ * value: value to search
+ * Retun: index of value or -1
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if ( array == NULL )
+		return -1;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return i;
+	}
+	return -1;
+}
