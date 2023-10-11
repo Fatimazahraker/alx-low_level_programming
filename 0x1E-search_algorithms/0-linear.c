1b0ffa30a8ec4423436df0623a548ecaec8b4297
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-10-11 00:02:53 -0700

    task

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -4,24 +4,24 @@
 
 /**
  * linear_search - search for value in an array
- * array: pointer to array
- * size: size of array
- * value: value to search
- * Retun: index of value or -1
+ * @array: pointer to array
+ * @size: size of array
+ * @value: value to search
+ * Return: index of value or -1
  */
 
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if ( array == NULL )
-		return -1;
+	if (array == NULL)
+		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-			return i;
+			return (i);
 	}
-	return -1;
+	return (-1);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 #include "search_algos.h"
 #include <math.h>
+
 /**
- * main - Entry point
+ * binary_search - searches for a value in an integer array using a binary
+ * @array: pointer to first element of array to seach
+ * @size: number of elements in array
+ * @value: value to search for
  *
- * Return: Always EXIT_SUCCESS
- *
+ * Return: index containing `value`, or -1 if `value` not found
  */
 
 int binary_search(int *array, size_t size, int value)
@@ -14,21 +17,22 @@ int binary_search(int *array, size_t size, int value)
 	int i, j, start = 0, end = size - 1, midl;
 
 	if (array == NULL)
-		return -1;
+		return (-1);
 
 	for (i = start; start <= end; i++)
 	{
 		midl = (end + start) / 2;
 		printf("Searching in array:");
-		for ( j = start; j <= (end - 1); j++)
+		for (j = start; j <= (end - 1); j++)
 			printf(" %d,", array[j]);
 		printf(" %d\n", array[j]);
 		if (value < array[midl])
 			end = midl - 1;
-		else start = midl + 1;
+		else
+			start = midl + 1;
 		if (array[midl] == value)
-			return midl;
+			return (midl);
 
 	}
-	return -1;
+	return (-1);
 }
