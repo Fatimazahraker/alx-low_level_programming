9567364271ca05cbbce3b48f361a5d5a19c84371
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-10-10 23:47:51 -0700

    task

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -21,8 +21,8 @@ int binary_search(int *array, size_t size, int value)
 		midl = (end + start) / 2;
 		printf("Searching in array:");
 		for ( j = start; j <= (end - 1); j++)
-			printf("%d, ", array[j]);
-		printf("%d\n", array[j]);
+			printf(" %d,", array[j]);
+		printf(" %d\n", array[j]);
 		if (value < array[midl])
 			end = midl - 1;
 		else start = midl + 1;
